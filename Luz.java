ef0b732cef1591efc72390bcf01da3a86a304d7c
Luz199785 <111939240+Luz199785@users.noreply.github.com>
2022-11-28 08:48:08 -0500

    Add files via upload

---
+++ Luz.java
@@ -0,0 +1,48 @@
+                                                                                                                                                                                                                        /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.luz;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aprendiz
+ */
+public class Luz {
+
+    public static void main(String[] args) {
+     System.out.println("Bienvenidos estudiantes");
+        String n;
+     
+     String nombre = "Luz";
+     String apellido = "Moreno";
+     int edad = 800;
+     
+     System.out.println("Hola " + nombre + " "+ apellido);
+     System.out.println("Edad " + edad);
+       
+       //operadores aritmeticos
+       
+       int valor1 = 20;
+       int valor2 = 30;
+       
+       // SUMA +
+       // RESTA -
+       // MULTIPLICACIÓN *
+       // DIVISION /
+       
+       int suma = valor1 + valor2;
+       int resta = valor1 - valor2;
+       int multi = valor1 * valor2;
+       int div = valor1 / valor2;
+       
+       System.out.println ("Suma " + suma);
+       System.out.println("resta" + resta);
+       System.out.println("multiplicaciín" + multi);
+       System.out.println("division" + div);
+                   
+    }
+}
